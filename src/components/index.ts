cdc54c60898c68c7b5a48318bc571114fbf84365
biostunt <nice.nikita99@mail.ru>
2021-07-20 20:47:10 +0300

    update component imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
-export { default as App } from './app/app.component';
+import App from './app/app.component';
+export { default as Header } from './header';
+export {default as Footer} from './footer'
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import reactDom from 'react-dom';
 import './styles/globals.css';
-import { App } from './components';
-
+import App from './components'
 
 reactDom.render(<App/>, document.getElementById("app"));
