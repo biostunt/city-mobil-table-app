0ceabecdeafbdb24a63cfb2f7a5a10a4a057031f
biostunt <nice.nikita99@mail.ru>
2021-07-20 20:46:50 +0300

    update app layout

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,7 +1,20 @@
-const App = () => {
-    return <div className="">
-        Hello world!
+import style from '../../styles/app.module.css';
+import Header from '../header';
+import Footer from '../footer';
+
+const App = () => (
+    <div className={style.container}>
+        <div className={style.header}>
+            <Header/>
+        </div>
+        <div className={style.body}>
+            <div className={style.sidebar}>sidebar</div>
+            <div className={style.table}>table</div>
+        </div>
+        <div className={style.footer}>
+            <Footer/>
+        </div>
     </div>
-}
+)
 
 export default App;
