3489649e2f25c866daa2cc22ef57bc981750b1eb
biostunt <nice.nikita99@mail.ru>
2021-07-20 20:46:16 +0300

    create footer component

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from '../../styles/footer.module.css';
+
+const Footer = () => (
+    <div className={style.container}>
+        <div className="block-description">footer</div>
+    </div>
+)
+
+export default React.memo(Footer);
