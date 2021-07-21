e77fc9ee87fac3e2d79cda09fbeaa341afa30980
biostunt <nice.nikita99@mail.ru>
2021-07-21 16:23:08 +0300

    create search switcher to live search

--- src/components/search/search.component.tsx
+++ src/components/search/search.component.tsx
@@ -1,25 +1,43 @@
+import { useState } from 'react';
 import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {IStoreState, storeActions} from '../../store'
-
 import style from '../../styles/search.module.css';
 
-const Search = () => {
+const DYNAMIC_SEARCH = false;
+
+
+const DynamicSearch = () => {
     
     const value = useSelector((state: IStoreState) => state.search.value);
     const dispatch = useDispatch();
     const input = useRef<HTMLInputElement>(null);
 
-    const onChange = () => dispatch(storeActions.search.onInputChanged(input?.current?.value || ""));
+    const onChange = () => dispatch(storeActions.search.onInputChanged(input.current?.value || ""));
 
+    return <div className={style.container}>
+        <div className={style.search_area_dynamic}>
+            <input ref={input} placeholder='Поиск' className={style.search_field} value={value} name='vehicle-search' onChange={onChange}></input>
+        </div>
+    </div>
+}
+
+const StaticSearch = () => {
+    const [value, setValue] = useState("");
+    const input = useRef<HTMLInputElement>(null);
+    const dispatch = useDispatch();
+    
+    const onChange = () => setValue(input.current?.value || "");
+    const onClick = () => dispatch(storeActions.search.onInputChanged(value))
+    
     return <div className={style.container}>
         <div className={style.search_area}>
             <input ref={input} placeholder='Поиск' className={style.search_field} value={value} name='vehicle-search' onChange={onChange}></input>
         </div>
         <div className={style.button_area}>
-            <div className={style.button}>Найти</div>
+            <div className={style.button} onClick={onClick}>Найти</div>
         </div>
     </div>
 }
 
-export default Search;
+export default DYNAMIC_SEARCH ? DynamicSearch : StaticSearch;
