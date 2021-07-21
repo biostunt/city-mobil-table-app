48eeedb412e56c19b9380800f4ecb9bd7bfa865a
biostunt <nice.nikita99@mail.ru>
2021-07-21 16:31:47 +0300

    update project imports. fix app folder imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,9 @@
-import App from './app/app.component';
+import App from './app';
 export { default as Header } from './header';
 export {default as Footer} from './footer'
+export { default as SideBar } from './sidebar';
+export { default as Search } from './search';
+export { default as Table } from './table';
+export { default as Vehicle } from './vehicle';
 
 export default App;
