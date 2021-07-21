5a2585a041894f06651464691119fcffd4dd1d1d
biostunt <nice.nikita99@mail.ru>
2021-07-21 11:48:05 +0300

    update output action input argument

--- src/store/output/actions.ts
+++ src/store/output/actions.ts
@@ -1,10 +1,17 @@
-import { IVehicle } from '../../api/cars';
 import TYPE from './types';
+
+export interface IVehicle {
+    mark: string;
+    model: string;
+    year: number;
+}
 
 export interface IOutputAction {
     type: string;
     payload: IVehicle
 }
+
+
 
 const onVehicleChoosed = (vehicle: IVehicle) => ({ type: TYPE.ON_VEHICLE_CHOOSED, payload: vehicle });
 
