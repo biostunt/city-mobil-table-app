7ada9efe8b0ec030d54f15f94a9e5665ec338c87
biostunt <nice.nikita99@mail.ru>
2021-07-21 11:14:54 +0300

    add redux provider to search component

--- src/components/search/search.component.tsx
+++ src/components/search/search.component.tsx
@@ -1,11 +1,20 @@
+import { useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {IStoreState, storeActions} from '../../store'
 
 import style from '../../styles/search.module.css';
 
 const Search = () => {
     
+    const value = useSelector((state: IStoreState) => state.search.value);
+    const dispatch = useDispatch();
+    const input = useRef<HTMLInputElement>(null);
+
+    const onChange = () => dispatch(storeActions.search.onInputChanged(input?.current?.value || ""));
+
     return <div className={style.container}>
         <div className={style.search_area}>
-            <input placeholder='Поиск' className={style.search_field} name='vehicle-search'></input>
+            <input ref={input} placeholder='Поиск' className={style.search_field} value={value} name='vehicle-search' onChange={onChange}></input>
         </div>
         <div className={style.button_area}>
             <div className={style.button}>Найти</div>
