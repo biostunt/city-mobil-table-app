6253f364c29e3caa8bff2902e8d7a7fe73325836
biostunt <nice.nikita99@mail.ru>
2021-07-21 11:45:55 +0300

    attach store to vehicle component

--- src/components/vehicle/vehicle.component.tsx
+++ src/components/vehicle/vehicle.component.tsx
@@ -1,11 +1,26 @@
+import { useSelector } from 'react-redux';
+import {IStoreState} from '../../store'
 import style from '../../styles/vehicle.module.css';
+
+
+const outTemplates = {
+    EMPTY: 'Машина не выбрана. Для просмотра выберите машину, приведенную в списке',
+    FULL: 'Выбран автомобиль mark model year года выпуска'
+}
 
 const Vehicle = () => {
 
-    const text: string = "Выбрана машина такая-то";
+
+
+    const vehicle = useSelector((state: IStoreState) => state.output.vehicle);
+    const text = vehicle ?
+        Object.entries(vehicle).reduce((prev: string, [key, value]: any) => prev.replace(key, `${value}`), outTemplates.FULL)
+        : outTemplates.EMPTY;
 
     return <div className={style.container}>
-        <div className={style.text}>{text}</div>
+        <div className={style.text}>{
+            text
+        }</div>
     </div>
 }
 
